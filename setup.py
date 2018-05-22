eeb6dbf0d3d52246ebdc328f53d284c6f9b4180e
Dmitry Ivanov <ivadmi5@gmail.com>
2018-05-22 21:53:22 +0300

    add missing dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup
 
+install_requires=['toml']
+
 setup(
     name='norsu',
     version='0.1',
     packages=['norsu'],
     license='PostgreSQL',
+    install_requires=install_requires,
     entry_points={
         'console_scripts': ['norsu = norsu.main:main']
     }
