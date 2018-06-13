e182c6229118b626edac21968337822ccfd2493d
Dmitry Ivanov <ivadmi5@gmail.com>
2018-06-13 13:19:52 +0300

    improve version management

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import norsu
 from setuptools import setup
 
 install_requires = ['toml', 'testgres']
 
 setup(
     name='norsu',
-    version='0.2',
+    version=norsu.__version__,
     packages=['norsu'],
     package_data={'norsu': ['data/*']},
     license='MIT',
