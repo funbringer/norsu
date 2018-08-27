c78ea18c26eff78b087b67aa52de2e9f4b9e61b2
Dmitry Ivanov <ivadmi5@gmail.com>
2018-08-27 23:11:30 +0300

    run: add test for this command

--- tests/test_basic.sh
+++ tests/test_basic.sh
@@ -8,6 +8,11 @@ norsu install master --extensions auto_explain
 # show configure flags
 "$(norsu path master)/bin/pg_config" > /dev/null
 
+# run a query
+echo show max_connections | norsu run master \
+	--psql --config <(echo max_connections = 25) \
+	-- -Xatq 2> /dev/null
+
 # remove only installation
 rm -rf "$(norsu path master)"
 
