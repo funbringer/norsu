73891400ba6f365532cd7e908ff4bc44f19dcf57
Dmitry Ivanov <ivadmi5@gmail.com>
2019-08-30 23:42:04 +0300

    Fix issues found by shellcheck

--- tester.sh
+++ tester.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2059,SC2140
 
-TEST_DIR=$(dirname "$0")
-export NORSU_PATH=$TEST_DIR/pg
+TEST_DIR="$(dirname "$0")"
+export NORSU_PATH="$TEST_DIR/pg"
 
 
 handler() {
-	rm -rf "$NORSU_PATH"
-	exit 1
+    rm -rf "$NORSU_PATH"
+    exit 1
 }
 
 trap handler SIGINT
@@ -18,15 +19,15 @@ rm -rf "$TEST_DIR/regression.diffs"
 
 
 if [ -t 1 ]; then
-	RED="\033[31m"
-	GREEN="\033[32m"
-	YELLOW="\033[33m"
-	RESET="\033[0m"
+    RED="\033[31m"
+    GREEN="\033[32m"
+    YELLOW="\033[33m"
+    RESET="\033[0m"
 else
-	RED=
-	GREEN=
-	YELLOW=
-	RESET=
+    RED=
+    GREEN=
+    YELLOW=
+    RESET=
 fi
 
 
@@ -41,42 +42,42 @@ printf "$YELLOW===== Tests: =====$RESET\\n"
 echo
 
 for t in "$TEST_DIR"/tests/*; do
-	NAME="$(basename "$t")"
-	OUT="${NAME%.*}.out"
+    NAME="$(basename "$t")"
+    OUT="${NAME%.*}.out"
 
-	EXPECTED="$TEST_DIR/expected/$OUT"
-	RESULT="$TEST_DIR/results/$OUT"
+    EXPECTED="$TEST_DIR/expected/$OUT"
+    RESULT="$TEST_DIR/results/$OUT"
 
-	printf "Running test %s ... " "$NAME"
-	bash "$t" > "$RESULT" 2>&1
+    printf "Running test %s ... " "$NAME"
+    bash "$t" > "$RESULT" 2>&1
 
-	TESTS_TOTAL=$((TESTS_TOTAL + 1))
+    TESTS_TOTAL=$((TESTS_TOTAL + 1))
 
-	if [ -f "$EXPECTED" ]; then
-		DIFF="$(diff -u "$EXPECTED" "$RESULT")"
+    if [ -f "$EXPECTED" ]; then
+        DIFF="$(diff -u "$EXPECTED" "$RESULT")"
 
-		if [ "$DIFF" == "" ]; then
-			printf "$GREEN"OK"$RESET\\n"
-			TESTS_GOOD=$((TESTS_GOOD + 1))
-		else
-			printf "$RED"FAIL"$RESET\\n"
-			TESTS_BAD=$((TESTS_BAD + 1))
+        if [ "$DIFF" == "" ]; then
+            printf "$GREEN"OK"$RESET\\n"
+            TESTS_GOOD=$((TESTS_GOOD + 1))
+        else
+            printf "$RED"FAIL"$RESET\\n"
+            TESTS_BAD=$((TESTS_BAD + 1))
 
-			# append diff to regression report
-			echo "$DIFF" >> "$TEST_DIR/regression.diffs"
-		fi
-	else
-		echo
-		printf "\\tmissing output file %s\\n" "$EXPECTED" >&2
-	fi
+            # append diff to regression report
+            echo "$DIFF" >> "$TEST_DIR/regression.diffs"
+        fi
+    else
+        echo
+        printf "\\tmissing output file %s\\n" "$EXPECTED" >&2
+    fi
 done
 
 # show report, if any
 if [ -f regression.diffs ]; then
-	echo
-	printf "$YELLOW===== Diffs: =====$RESET\\n"
-	echo
-	cat regression.diffs
+    echo
+    printf "$YELLOW===== Diffs: =====$RESET\\n"
+    echo
+    cat regression.diffs
 fi
 
 echo
@@ -90,5 +91,5 @@ echo
 
 # report errors using non-zero exit code
 if [ "$TESTS_GOOD" -lt "$TESTS_TOTAL" ]; then
-	exit 1
+    exit 1
 fi
